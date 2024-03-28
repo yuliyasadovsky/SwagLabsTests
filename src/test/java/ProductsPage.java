fd800f42a324eafda584c8c135244b547c680a15
Yuliya Sadovsky <yuliya.sadovsky@gmail.com>
2024-03-28 17:57:53 +0300

    ProductsPageTests added

---
+++ src/test/java/ProductsPage.java
@@ -0,0 +1,84 @@
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import static com.codeborne.selenide.Selenide.$$x;
+import static com.codeborne.selenide.Selenide.$x;
+
+
+public class ProductsPage {
+
+   // private final ElementsCollection productsList = $$x("//div[@class = \"inventory_item\"]");
+    private final ElementsCollection productsDescriptionList = $$x("//div[@class = \"inventory_item_name \"]");
+    private final ElementsCollection addToCartButtons = $$x("//button[@class = \"btn btn_primary btn_small btn_inventory \"]");
+    private final SelenideElement shoppingCartBadge = $x("//span[@class = \"shopping_cart_badge\"]");
+    private final ElementsCollection priceLabels = $$x("//div[@class = \"inventory_item_price\"]");
+    private final SelenideElement filterButton = $x("//select");
+    private final SelenideElement filterOptionAZ = $x("//select//option[@value = \"az\"]");
+    private final SelenideElement filterOptionZA = $x("//select//option[@value = \"za\"]");
+    private final SelenideElement filterOptionLowHigh = $x("//select//option[@value = \"lohi\"]");
+    private final SelenideElement filterOptionHighLow = $x("//select//option[@value = \"hilo\"]");
+
+
+    public String getFirstProductDescription() {
+        return productsDescriptionList.first().text();
+    }
+
+    public String  getLastProductDescription() {
+        return productsDescriptionList.last().text();
+    }
+    public SelenideElement getShoppingCartBadge() {
+        return shoppingCartBadge;
+    }
+
+    public SelenideElement getFirstProductPrice() {
+        return priceLabels.first();
+    }
+
+    public SelenideElement getLastProductPrice() {
+        return priceLabels.last();
+    }
+    public void clickOnAddToCartButton() {
+        addToCartButtons.first().click();
+    }
+
+    public ProductsPage clickOnFilterButton() {
+        filterButton.click();
+        return new ProductsPage();
+    }
+
+    public void setFilterAZ() {
+        filterOptionAZ.click();
+    }
+
+    public void setFilterZA() {
+        filterOptionZA.click();
+    }
+
+    public void setFilterLowHigh() {
+        filterOptionLowHigh.click();
+    }
+
+    public void setFilterHighLow() {
+        filterOptionHighLow.click();
+    }
+
+    public boolean areStringsSortedAlphabetically(String str1, String str2) {
+        int minLength = Math.min(str1.length(), str2.length());
+
+        for (int i = 0; i < minLength; i++) {
+            char char1 = Character.toLowerCase(str1.charAt(i));
+            char char2 = Character.toLowerCase(str2.charAt(i));
+
+            if (char1 != char2) {
+                return char1 < char2;
+            }
+        }
+
+        return str1.length() <= str2.length();
+    }
+
+    public boolean isFirstPriceLessThanSecond(String price1, String price2) {
+        double num1 = Double.parseDouble(price1.substring(1));
+        double num2 = Double.parseDouble(price2.substring(1));
+        return num1 < num2;
+    }
+}
---
+++ src/test/java/ProductsPageTests.java
@@ -0,0 +1,66 @@
+import com.codeborne.selenide.AssertionMode;
+import com.codeborne.selenide.Condition;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+public class ProductsPageTests extends BaseTest {
+    private final static String BASE_URL = "https://www.saucedemo.com/";
+    private final static String USER_NAME = "standard_user";
+    private final static String USER_PASSWORD = "secret_sauce";
+
+    @Before
+    public void login() {
+        LoginPage loginPage = new LoginPage(BASE_URL);
+        loginPage.setUsername(USER_NAME);
+        loginPage.setPassword(USER_PASSWORD);
+        loginPage.clickLogin();
+    }
+
+    @Test
+    public void addToCartBadgeTest() {
+        ProductsPage productsPage = new ProductsPage();
+        productsPage.clickOnAddToCartButton();
+        Assert.assertTrue(productsPage.getShoppingCartBadge().should().exists());
+    }
+
+    @Test
+    public void firstProductHasPriceTest() {
+        ProductsPage productsPage = new ProductsPage();
+        Assert.assertTrue(productsPage.getFirstProductPrice().should().exists());
+    }
+
+    @Test
+    public void filterByNameFromAtoZTest() {
+        ProductsPage productsPage = new ProductsPage();
+        productsPage.clickOnFilterButton().setFilterAZ();
+        Assert.assertTrue(productsPage.areStringsSortedAlphabetically(productsPage.getFirstProductDescription(),
+                productsPage.getLastProductDescription()));
+    }
+
+    @Test
+    public void filterByNameFromZtoATest() {
+        ProductsPage productsPage = new ProductsPage();
+        productsPage.clickOnFilterButton().setFilterZA();
+        Assert.assertTrue(productsPage.areStringsSortedAlphabetically(productsPage.getLastProductDescription(),
+                productsPage.getFirstProductDescription()));
+    }
+
+    @Test
+    public void filterByPriceFromLowToHigh() {
+        ProductsPage productsPage = new ProductsPage();
+        productsPage.clickOnFilterButton().setFilterLowHigh();
+        Assert.assertTrue(productsPage.isFirstPriceLessThanSecond(productsPage.getFirstProductPrice().text(),
+                productsPage.getLastProductPrice().text()));
+    }
+
+    @Test
+    public void filterByPriceFromHighToLow() {
+        ProductsPage productsPage = new ProductsPage();
+        productsPage.clickOnFilterButton().setFilterHighLow();
+        Assert.assertTrue(productsPage.isFirstPriceLessThanSecond( productsPage.getLastProductPrice().text(),
+                productsPage.getFirstProductPrice().text()));
+    }
+}
